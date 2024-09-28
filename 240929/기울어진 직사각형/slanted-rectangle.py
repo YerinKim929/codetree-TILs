4fe11792dfc762a8a487eb0340598961db96493f
Yerin Kim <76947209+YerinKim929@users.noreply.github.com>
2024-09-29 01:23:35 +0900

    Title: 기울어진 직사각형, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240929/기울어진 직사각형/slanted-rectangle.py
@@ -0,0 +1,46 @@
+import sys
+
+def is_range(a, b):
+    return 0 <= a < n and 0 <= b < n
+
+def how_move(r, c, move_r, move_c):
+
+    line_sum = 0
+
+    dx = [-1, -1, 1, 1]
+    dy = [1, -1, -1 ,1]
+
+    for d in range(4):
+        N = move_r+1
+        if d==0 or d==2:
+            N = move_c+1
+
+        for i in range(N):
+            if not is_range(r+dx[d], c+dy[d]):
+                return -1
+            line_sum += board[r+dx[d]][c+dy[d]]
+            r += dx[d]
+            c += dy[d]
+
+    return line_sum
+
+n = int(sys.stdin.readline())
+
+board = [
+    list(map(int, sys.stdin.readline().split()))
+    for _ in range(n)
+]
+
+max_sum = 0
+
+for r in range(2, n):
+    for c in range(1, n):
+        # 1이 시작하는 위치 r c
+        for move_r in range(n-r):
+            for move_c in range(n-c):
+                # 얼마나 움직일 지 정하기 
+                line_sum = how_move(r, c, move_r, move_c)
+                max_sum = max(max_sum, line_sum)
+
+
+print(max_sum)
