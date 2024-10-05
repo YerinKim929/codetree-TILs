5de503115dff475eb63f5e4efaf292471632036c
Yerin Kim <76947209+YerinKim929@users.noreply.github.com>
2024-10-05 10:40:06 +0900

    Title: 피보나치 수, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 241005/피보나치 수/fibonacci-number.py
+++ 241005/피보나치 수/fibonacci-number.py
@@ -2,11 +2,16 @@ import sys
 
 N = int(sys.stdin.readline())
 
-dp = [0]*(N+1)
-dp[1] = 1
-dp[2] = 1
+memo = [-1]*(N+1)
 
-for i in range(3, N+1):
-    dp[i] = dp[i - 1] + dp[i - 2]
+def fibbo(n):
+    if memo[n] != -1:        
+        return memo[n]       
+    if n <= 2:               
+        memo[n] = 1          
+    else:                   
+        memo[n] = fibbo(n - 1) + fibbo(n - 2)  
+                                               
+    return memo[n]                
 
-print(dp[N])
+print(fibbo(N))
