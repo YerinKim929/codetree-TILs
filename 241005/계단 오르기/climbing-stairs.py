06c88278e1871c47c50e47dbac8d88f3c3b499e9
Yerin Kim <76947209+YerinKim929@users.noreply.github.com>
2024-10-05 18:09:44 +0900

    Title: 계단 오르기, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 241005/계단 오르기/climbing-stairs.py
+++ 241005/계단 오르기/climbing-stairs.py
@@ -2,11 +2,12 @@ n = int(input())
 MOD = 10007
 
 dp = [0] * (n+1)
+dp[0] = 1
 
 for i in range(2, n + 1):
-    if i == 2 or i == 3:
+    if i < 5:
         dp[i] = 1
         continue 
-    dp[i] = dp[i - 2] + dp[i - 3]%MOD
+    dp[i] = (dp[i - 2] + dp[i - 3]) % MOD
 
 print(dp[n])
