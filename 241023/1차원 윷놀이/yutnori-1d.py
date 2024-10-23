58fc688f82037c990246a9c28179b75faecb92e4
Yerin Kim <76947209+YerinKim929@users.noreply.github.com>
2024-10-23 20:28:53 +0900

    Title: 1차원 윷놀이, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241023/1차원 윷놀이/yutnori-1d.py
+++ 241023/1차원 윷놀이/yutnori-1d.py
@@ -1,6 +1,7 @@
 import sys
 import copy
 
+# i = 턴수 / goal = 말의 위치
 def choose(i, goal):
     global answer 
 
@@ -12,14 +13,16 @@ def choose(i, goal):
         answer = max(answer, tmp)
         return 
 
+    # 각 말마다 한턴씩 부여해서 주는 거지...
     for j in range(0, k):
-        copy_goal = copy.deepcopy(goal)
-        copy_goal[j] += nums[i]
-        choose(i+1, copy_goal)
+        if goal[j] < m:
+            copy_goal = copy.deepcopy(goal)
+            copy_goal[j] += nums[i]
+            choose(i+1, copy_goal)
 
 n, m, k = map(int, sys.stdin.readline().split())
 nums = list(map(int, sys.stdin.readline().split()))
 answer = 0
 
-choose(0, [1]*n)
+choose(0, [1]*k)
 print(answer)
