e91b897bdf42172c153a8798065806b9e2a4c94c
Yerin Kim <76947209+YerinKim929@users.noreply.github.com>
2024-10-24 21:11:22 +0900

    Title: 외판원 순회, Time: 105ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 241024/외판원 순회/traveling-salesman-problem.py
@@ -0,0 +1,35 @@
+import sys
+
+def choose(i):
+    global result
+
+
+    for j in range(n):
+
+        if sum(visited) == n and j == 0:
+            answer.append(board[i][j])
+            tmp = sum(answer)
+            if result == 0 or result > tmp:
+                result = tmp
+            answer.pop()
+            return
+
+        if i != j and not visited[j]:
+            answer.append(board[i][j])
+            visited[j] = 1
+            choose(j)
+            answer.pop()
+            visited[j] = 0
+
+n = int(input())
+board = [
+    list(map(int, sys.stdin.readline().split()))
+    for _ in range(n)
+]
+visited = [0]*n
+visited[0] = 1
+answer = []
+result = 0
+
+choose(0)
+print(result)
