e3ccd12dd951a2dca667a39f2c8e6e245c0a600c
Yerin Kim <76947209+YerinKim929@users.noreply.github.com>
2024-10-24 12:37:06 +0900

    Title: xor 결과 최대 만들기, Time: 287ms, Memory: 29MB, Status: Passed - CodeTree

--- 241024/xor 결과 최대 만들기/max-of-xor.py
+++ 241024/xor 결과 최대 만들기/max-of-xor.py
@@ -1,6 +1,6 @@
 import sys
 
-def choose_nums(i, xor_nums):
+def choose_nums(i, idx, xor_nums):
 
     global result
 
@@ -12,14 +12,15 @@ def choose_nums(i, xor_nums):
             result = xor
         return 
 
-    for j in range(0, n):
-        xor_nums.append(nums[j])
-        choose_nums(i+1, xor_nums)
+    if idx < n:
+        xor_nums.append(nums[idx])
+        choose_nums(i+1, idx+1, xor_nums)
         xor_nums.pop()
+        choose_nums(i, idx+1, xor_nums)
 
 n, m = map(int, sys.stdin.readline().split())
 nums = list(map(int, sys.stdin.readline().split()))
 
 result = 0
-choose_nums(0, [])
+choose_nums(0, 0, [])
 print(result)
