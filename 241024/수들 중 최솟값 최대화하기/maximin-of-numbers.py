dd0381eb19ba8c3873ba8e5cf87bd41010cacb4c
Yerin Kim <76947209+YerinKim929@users.noreply.github.com>
2024-10-24 21:45:29 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 263ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 241024/수들 중 최솟값 최대화하기/maximin-of-numbers.py
@@ -0,0 +1,37 @@
+import sys
+
+def choose(i, r):
+
+    global result
+
+    if i == n:
+        result = max(result, min(answer))
+        return
+
+    
+    if not visited_r[r]:
+        for c in range(n):
+            if not visited_c[c]:
+                answer.append(board[r][c])
+                visited_r[r] = 1
+                visited_c[c] = 1
+
+                choose(i+1, r+1)
+
+                answer.pop()
+                visited_r[r] = 0
+                visited_c[c] = 0
+    
+
+n = int(input())
+board = [
+    list(map(int, sys.stdin.readline().split()))
+    for _ in range(n)
+]
+visited_r = [0]*n
+visited_c = [0]*n
+answer = []
+result = 0
+
+choose(0, 0)
+print(result)
