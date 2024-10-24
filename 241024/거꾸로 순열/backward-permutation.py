54a88720cc006a1716f57c04bc7889593af672ab
Yerin Kim <76947209+YerinKim929@users.noreply.github.com>
2024-10-24 20:10:18 +0900

    Title: 거꾸로 순열, Time: 222ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 241024/거꾸로 순열/backward-permutation.py
@@ -0,0 +1,27 @@
+n = int(input())
+visited = [0] * (n + 1)
+answer = []
+
+def print_answer():
+    for elem in answer:
+        print(elem, end=" ")
+    print()
+
+def choose(curr_num):
+    if curr_num == n + 1:
+        print_answer()
+        return
+
+    for i in range(n, 0, -1):
+        if visited[i]:
+            continue
+
+        visited[i] = 1
+        answer.append(i)
+
+        choose(curr_num + 1)
+
+        answer.pop()
+        visited[i] = 0
+
+choose(1)
